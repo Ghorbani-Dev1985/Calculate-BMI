9a59110eb80bee730a20a27f5221c6e1f6e79f66
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-23 18:33:32 +0330

    feat: select elemnts in js file

---
+++ JS/Main.js
@@ -0,0 +1,6 @@
+let $ = document;
+let HeightRange = $.querySelector(".HeightRange");
+let WeightRange = $.querySelector(".WeightRange");
+let HeightValue = $.querySelector(".HeightValue");
+let WeightValue = $.querySelector(".WeightValue");
+let ResultBody = $.querySelector(".ResultBody");
